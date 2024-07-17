c355cb33725b25bd4e78e69877e30461f1d4b00b
lockrr <hemistergt@gmail.com>
2024-07-17 11:36:23 +0700

    deploy git

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const { removeBackground } = require('@imgly/background-removal-node');
+const { removeBackground } = require('@imgly/background-removal-node');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
@@ -15,22 +15,21 @@ app.use(bodyParser.json({ limit: '50mb' }));
 const upload = multer({ dest: 'uploads/' });
 
 app.post('/remove-background', upload.single('image'), async (req, res) => {
-  // try {
-  //   const { file } = req;
-  //   const imgPath = path.resolve(file.path);
-  //   const blob = await removeBackground(imgPath);
-  //   const buffer = Buffer.from(await blob.arrayBuffer());
-  //   const dataURL = `data:image/png;base64,${buffer.toString('base64')}`;
+  try {
+    const { file } = req;
+    const imgPath = path.resolve(file.path);
+    const blob = await removeBackground(imgPath);
+    const buffer = Buffer.from(await blob.arrayBuffer());
+    const dataURL = `data:image/png;base64,${buffer.toString('base64')}`;
 
-  //   // Clean up uploaded file
-  //   fs.unlinkSync(imgPath);
+    // Clean up uploaded file
+    fs.unlinkSync(imgPath);
 
-  //   res.json({ dataURL });
-  // } catch (error) {
-  //   console.error('Error removing background:', error);
-  //   res.status(500).send('Error removing background');
-  // }
-  res.status(200).send('tes');
+    res.json({ dataURL });
+  } catch (error) {
+    console.error('Error removing background:', error);
+    res.status(500).send('Error removing background');
+  }
 });
 
 app.listen(port, () => {
